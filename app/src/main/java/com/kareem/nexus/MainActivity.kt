453999e28a0ff5d85627ef36febad9f78664b0b4
KAN1409 <engkareemnasser@gmail.com>
2026-09-10 14:16:29 +0300

    NEXUS Update 1: wire product app shell

--- app/src/main/java/com/kareem/nexus/MainActivity.kt
+++ app/src/main/java/com/kareem/nexus/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.kareem.nexus.ui.HomeScreen
+import com.kareem.nexus.ui.NexusApp
 import com.kareem.nexus.ui.theme.NexusTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +13,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { NexusTheme { HomeScreen() } }
+        setContent {
+            NexusTheme {
+                NexusApp()
+            }
+        }
     }
 }

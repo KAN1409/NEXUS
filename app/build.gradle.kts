a5f27567b2077418eda6ed1ec7d96e5cb8dd63b3
KAN1409 <engkareemnasser@gmail.com>
2026-09-11 11:17:31 +0300

    NEXUS 1.0: bump major release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kareem.nexus"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0-u2"
+        versionCode = 100
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
